de49b59fea816265e30e50acf50fd440a47be649
Jeffery Amankwah <jefferyasanteamankwah@gmail.com>
2026-05-31 22:14:37 +0000

    Assignment 1 - My initial

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
 
 def main():
-  pass #code goes here
+    # File: initials.py
+    # Description: Print out my initials.
+    # Assignment Number: 1
+    #
+    # Name: Jeffery Asante Amankwah
+    # STUDENT ID:2425403230
+    # Email: jefferyasanteamankwah@gmail.com
+    # Grader: Carolyn
+    #
+    # On my honor, Jeffery Asante Amankwah, this programming assignment is my own work
+    # and I have not provided this code to any other student.
+    print()
+    print("...JAA")
+    print()
+    print("...JJJJJJJJJJJJ........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("............JJJ........AAA......AAA........AAA......AAA.....")
+    print("............JJJ........AAA......AAA........AAA......AAA.....")
+    print("............JJJ........AAA......AAA........AAA......AAA.....")
+    print("............JJJ........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("............JJJ........AAA......AAA........AAA......AAA.....")
+    print("............JJJ........AAA......AAA........AAA......AAA.....")
+    print("............JJJ........AAA......AAA........AAA......AAA.....")
+    print("...JJJ......JJJ...**...AAA......AAA...**...AAA......AAA...**")
+    print("...JJJJJJJJJJJJ...**...AAA......AAA...**...AAA......AAA...**")
+    print()
 
 
-
-main()
